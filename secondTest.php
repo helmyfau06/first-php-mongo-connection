17d826812d3877bb90c3640ad6b9d640dea38ec9
Helmy Fauzi <66837673+helmyfau06@users.noreply.github.com>
2022-11-19 16:32:16 +0700

    First Commit - First Connection mongodb using php

---
+++ secondTest.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php'; // include Composer's autoloader
+
+$manager = new MongoDB\Driver\Manager ("mongodb+srv://miwpaw:12345@cluster0.znihgih.mongodb.net/?retryWrites=true&w=majority");
+
+// Query Class
+$query = new MongoDB\Driver\Query(array("restaurant_id" => "40356018"));
+
+// Select Database Collection
+$cursor = $manager->executeQuery('sample_resataurants.restaurants', $query);
+
+// Convert cursor to array and print results
+echo "<pre>";
+print_r($cursor->toArray());
+echo "<pre>";
+
+?>
